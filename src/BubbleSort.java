e5c18de33bc4998e27567c43b8e85c5f815475f9
Arjun Sharma <asharm33@ncsu.edu>
2017-10-17 04:12:24 -0400

    BubbleSort

---
+++ src/BubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+/**
+ * @author asharm33
+ */
+public class BubbleSort {
+	public static void main(String argsd[]) {
+		int a[] = new int[]{7,6,9,5};
+		int noOfComparisons = 0;
+		int noOfPasses = 0;
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = a.length - 1; j > i; j--) {
+				noOfPasses++;
+				if (a[j] < a[j - 1]) {
+					noOfComparisons++;
+					int temp = a[j];
+					a[j] = a[j - 1];
+					a[j - 1] = temp;
+				}
+			}
+		}
+		System.out.println("No of passes " + noOfPasses);
+		System.out.println("No of Comparisons " + noOfComparisons);
+		System.out.println(Arrays.toString(a));
+	}
+}
