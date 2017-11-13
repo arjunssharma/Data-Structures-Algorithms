69e8151b73112cab14da7d5d530e74e3e79f8d78
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-13 18:12:33 -0500

    Implement strStr().
    Return the index of the first occurrence of needle in haystack, or -1 if
    needle is not part of haystack.

---
+++ src/leetcode/ImplementStrStr.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class ImplementStrStr {
+
+	public static void main(String args[]) {
+		System.out.println(strStr("mississippi", "issip"));
+	}
+
+	public static int strStr(String haystack, String needle) {
+		for (int i = 0;; i++) {
+			for (int j = 0;; j++) {
+				if (j == needle.length())
+					return i;
+				if (i + j == haystack.length())
+					return -1;
+				if (needle.charAt(j) != haystack.charAt(i + j))
+					break;
+			}
+		}
+	}
+}
