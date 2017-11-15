c23af839a6f33c5fb1bdff2d7362e08523a022f8
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-14 23:04:02 -0500

    Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.

---
+++ src/ctci/arrays_strings/_4_PalindromePermutation.java
@@ -0,0 +1,23 @@
+package ctci.arrays_strings;
+
+public class _4_PalindromePermutation {
+
+	public static void main(String args[]) {
+		System.out.println(canPermutePalindrome("ARJAR"));
+	}
+
+	public static boolean canPermutePalindrome(String str) {
+		// Assuming ASCII
+		char map[] = new char[128];
+		for (int i = 0; i < str.length(); i++) {
+			map[str.charAt(i)]++;
+		}
+
+		int count = 0;
+		for (int i = 0; i < map.length && count <= 1; i++) {
+			count += map[i] % 2;
+		}
+
+		return count <= 1;
+	}
+}
