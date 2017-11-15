22d11b01a787fe80de2a566d9fef023340649e06
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-15 00:00:27 -0500

    One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.

---
+++ src/ctci/arrays_strings/_5_OneAway.java
@@ -0,0 +1,31 @@
+package ctci.arrays_strings;
+
+public class _5_OneAway {
+
+	public static void main(String args[]) {
+		System.out.println(checkEdit("bale", "kali"));
+	}
+
+	public static boolean checkEdit(String s, String t) {
+		if (s == null || t == null)
+		    return false;
+		
+		if (s.length() > t.length())
+		    return checkEdit(t, s);
+		      
+		  int i = 0, j = 0;
+		  //t should be the longer string
+		  while (i < s.length() && j < t.length()) {
+		    if (s.charAt(i) != t.charAt(j)) {
+		      // we try to replace s[i] with s[j] or insert s[j] to s[i]
+		      // then compare the rest and see if they are the same
+		      return s.substring(i + 1).equals(t.substring(j + 1)) ||
+		             s.substring(i).equals(t.substring(j + 1));
+		    }
+		    
+		    i++; j++;
+		  }
+		  
+		  return true;
+	}
+}
