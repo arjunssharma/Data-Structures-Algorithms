43c49d333d9bf94685bfee75ffe79887abd2e103
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-14 22:46:38 -0500

    URLify: Write a method to replace all spaces in a string with '%20: You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)

---
+++ src/ctci/arrays_strings/_3_URLify.java
@@ -0,0 +1,37 @@
+package ctci.arrays_strings;
+
+import java.util.Arrays;
+
+public class _3_URLify {
+
+	public static void main(String args[]) {
+		String s = "Mr John Smith     ";
+		char str[] = s.toCharArray();
+		replace(str, 13);
+		System.out.println(Arrays.toString(str));
+	}
+	
+	public static void replace(char str[], int length) {
+		int countOfSpaces = 0;
+		for (int i = 0; i < length; i++) {
+			if (str[i] == ' ')
+				countOfSpaces++;
+		}
+		
+		int index = length + countOfSpaces * 2;
+		if(length < str.length)
+			str[length] = '\0';
+		
+		for(int i = length - 1; i >= 0; i--) {
+			if(str[i] == ' ') {
+				str[index - 1]  = '0';
+				str[index - 2]  = '2';
+				str[index - 3]  = '%';
+				index = index - 3;
+			} else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+	}
+}
