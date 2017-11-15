eef0a04cbdf3da44da0bc631162e153a8d190446
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-15 00:14:22 -0500

    Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).

---
+++ src/ctci/arrays_strings/_6_StringCompression.java
@@ -0,0 +1,25 @@
+package ctci.arrays_strings;
+
+public class _6_StringCompression {
+
+	
+	public static void main(String args[]) {
+		String s = "aabcc";
+		System.out.println(compress(s));
+	}
+	
+	public static String compress(String s) {
+		StringBuilder compress = new StringBuilder();
+		int count = 0;
+		for(int i = 0; i < s.length(); i++) {
+			count++;
+			if(i + 1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+				compress.append(s.charAt(i));
+				compress.append(count);
+				count = 0;
+			}
+		}
+		
+		return compress.toString();
+	}
+}
