c3e4f38a7800461c1d0b1d1866bf5c8624fb3f55
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-21 00:55:48 -0500

    Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure SetOfStacks that mimics this. SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity. SetOfStacks. push () and SetOfStacks .pop () should behave identically to a single stack (that is, pop ( ) should return the same values as it would if there were just a single stack).

---
+++ src/ctci/stacks_queues/_3_StackOfPlates.java
@@ -0,0 +1,158 @@
+package ctci.stacks_queues;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class _3_StackOfPlates {
+
+	ArrayList<Stack> stacks = new ArrayList<Stack>();
+	private static final int CAPACITY = 3;
+	
+	public void push(int x) {
+		Stack last = getLastStack();
+		if(last != null && !last.isFull()) {
+			last.push(x);
+		}
+		else {
+			Stack stack = new Stack(CAPACITY);
+			stack.push(x);
+			stacks.add(stack);
+		}
+	}
+	
+	public int pop() {
+		Stack last = getLastStack();
+		if (last != null) {
+			int v = last.pop();
+			if (last.isEmpty()) { // last.size() == 0
+				stacks.remove(stacks.size() - 1);
+			}
+			return v;
+		} else {
+			throw new EmptyStackException();
+		}
+	}
+	
+	public int popAt(int index) {
+		return leftShift(index, true);
+	}
+	
+	public int leftShift(int index, boolean removeTop) {
+		Stack s = stacks.get(index);
+		int removed_item;
+		if(removeTop) {
+			removed_item = s.pop();
+		} else {
+			removed_item = s.removeBottom();
+		}
+		
+		if(s.isEmpty()) {
+			stacks.remove(index);
+		} else if(stacks.size() > index + 1) {
+			int v = leftShift(index + 1, false);
+			s.push(v);
+		}
+		return removed_item;
+	}
+	
+	public Stack getLastStack() {
+		if(!stacks.isEmpty() && stacks != null) {
+			return stacks.get(stacks.size() - 1);
+		} else {
+			return null;
+		}
+	}
+	
+	
+	public static void main(String args[]) {
+		_3_StackOfPlates plates = new _3_StackOfPlates();	
+		plates.push(5);
+		plates.push(5);
+		plates.push(5);
+		plates.push(15);
+		plates.push(15);
+		plates.push(15);
+		plates.push(25);
+		plates.push(25);
+		plates.push(25);
+		
+		System.out.println(plates.popAt(1));
+		System.out.println(plates.getLastStack().size);
+	}
+}
+
+class Stack{
+	
+	Node top;
+	Node bottom;
+	int capacity;
+	int size = 0;
+	
+	public Stack(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public boolean isFull() {
+		return capacity == size;
+	}
+	
+	public void join(Node above, Node below) {
+		if (below != null)
+			below.above = above;
+
+		if (above != null)
+			above.below = below;
+	}
+	
+	public boolean push(int x) {
+		if(capacity <= size) {
+			return false;
+		}
+		size++;
+		Node n = new Node(x);
+		if(size == 1) {
+			bottom = n;
+		}
+		join(n, top);
+		top = n;
+		return true;
+	}
+	
+	public int peek() {
+		return top.data;
+	}
+	
+	public int pop() {
+		Node n = top;
+		top = top.below; //And after this we can make n.below = null and top.above = null
+		size--;
+		return n.data;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public int removeBottom() {
+		Node b = bottom;
+		if(bottom != null)
+			bottom = bottom.above;
+		bottom.below = null;
+		size--;
+		return b.data;
+	}
+}
+
+class Node {
+	int data;
+	Node below;
+	Node above;
+	
+	Node() {
+
+	}
+
+	Node(int data) {
+		this.data = data;
+	}
+}
