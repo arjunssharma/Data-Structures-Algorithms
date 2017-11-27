e7d187db56d1e415faa8079f318f94a3c0c6408a
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-26 19:54:10 -0500

    Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.

---
+++ src/ctci/trees_graphs/_1_RouteBetweenNodes.java
@@ -0,0 +1,42 @@
+package ctci.trees_graphs;
+
+import java.util.LinkedList;
+
+import ctci.trees_graphs.Graph.Node;
+import ctci.trees_graphs.Graph.State;;
+
+public class _1_RouteBetweenNodes {
+
+	
+	public boolean search(Graph g, Node start, Node end) {
+		if(start == end)
+			return true;
+		
+		LinkedList<Node> q = new LinkedList<Node>();
+		
+		for(Node n : g.getNodes()) {
+			n.state = State.unvisited;
+		}
+		
+		start.state = State.visiting;
+		q.add(start);
+		Node u;
+		while(!q.isEmpty()) {
+			u = q.removeFirst();
+			if(u != null) {
+				for(Node v : u.getAdjacent()) {
+					if(v.state == State.unvisited) {
+						if(v == end)
+							return true;
+						else {
+							v.state = State.visiting;
+							q.add(v);
+						}
+					}
+				}
+				u.state = State.visited;
+			}
+		}
+		return false;
+	}
+}
